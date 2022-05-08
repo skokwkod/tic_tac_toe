f5926279eec35affc808ed09dfa1d8c5319d7550
Michal <michaldabek1990@gmail.com>
2022-05-08 14:05:17 +0200

    naprawa braku reakcji na remis

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -46,6 +45,7 @@ class _TicTacToeState extends State<TicTacToe> {
   bool enabled = true;
   String errorMessage = "Pole już zostało użyte!";
   List<int> score = [0, 0];
+  int selectedCount = 0;
 
   void _clearBoard() {
     setState(() {
@@ -63,12 +63,13 @@ class _TicTacToeState extends State<TicTacToe> {
       symbols = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
       xTurn = true;
       enabled = true;
+      selectedCount = 0;
     });
   }
 
   String _checkWinner() {
-    String winner = "";
-    String Function(int, int, int) checkPossibilities = (a, b, c) {
+    String Function(int, int, int) checkPossibilities;
+    checkPossibilities = (a, b, c) {
       if (symbols[a] != " ") {
         if ((symbols[a] == symbols[b]) && (symbols[b] == symbols[c])) {
           return symbols[a];
@@ -77,7 +78,9 @@ class _TicTacToeState extends State<TicTacToe> {
       return "";
     };
 
-    final posibilities = [
+    String winner = "";
+
+    final possibilities = [
       checkPossibilities(0, 1, 2),
       checkPossibilities(3, 4, 5),
       checkPossibilities(6, 7, 8),
@@ -88,9 +91,9 @@ class _TicTacToeState extends State<TicTacToe> {
       checkPossibilities(2, 4, 6)
     ];
 
-    for (int i = 0; i < posibilities.length; i++) {
-      if (posibilities[i] != "") {
-        winner = posibilities[i];
+    for (int i = 0; i < possibilities.length; i++) {
+      if (possibilities[i] != "") {
+        winner = possibilities[i];
         break;
       }
     }
@@ -98,6 +101,7 @@ class _TicTacToeState extends State<TicTacToe> {
   }
 
   void _selectedField(int index) {
+    selectedCount++;
     if (enabled == true) {
       setState(() {
         if (xTurn && !selected[index] && enabled) {
@@ -114,7 +118,6 @@ class _TicTacToeState extends State<TicTacToe> {
         }
       });
 
-
       if (_checkWinner() == "X") {
         print(_checkWinner());
         score[0] += 1;
@@ -127,6 +130,9 @@ class _TicTacToeState extends State<TicTacToe> {
         print(score);
         enabled = false;
       }
+      if (_checkWinner() == "DRAW") {
+        enabled = false;
+      }
     }
   }
 
@@ -136,14 +142,15 @@ class _TicTacToeState extends State<TicTacToe> {
       body: Center(
         child: Column(
           children: [
+            Expanded(flex: 1, child: SizedBox(height: 10)),
             Expanded(
-                flex:1,
-                child: SizedBox(height: 10)),
-            Expanded(
-              flex:1,
+              flex: 1,
               child: Container(
                 child: Text(
-                  "Wyniki\n" + score[0].toString() + " : " + score[1].toString(),
+                  "Wyniki\n" +
+                      score[0].toString() +
+                      " : " +
+                      score[1].toString(),
                   style: TextStyle(
                     fontSize: 40,
                   ),
@@ -154,7 +161,6 @@ class _TicTacToeState extends State<TicTacToe> {
             Expanded(
               flex: 6,
               child: Container(
-
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -162,20 +168,23 @@ class _TicTacToeState extends State<TicTacToe> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[0], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[0], animation: animated),
                             onTap: () {
                               animated = !animated;
                               _selectedField(0);
                               print("tap 1");
                             }),
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[1], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[1], animation: animated),
                             onTap: () {
                               _selectedField(1);
                               print("tap 2");
                             }),
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[2], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[2], animation: animated),
                             onTap: () {
                               _selectedField(2);
                               print("tap 3");
@@ -186,19 +195,22 @@ class _TicTacToeState extends State<TicTacToe> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[3], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[3], animation: animated),
                             onTap: () {
                               _selectedField(3);
                               print("tap 4");
                             }),
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[4], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[4], animation: animated),
                             onTap: () {
                               _selectedField(4);
                               print("tap 5");
                             }),
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[5], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[5], animation: animated),
                             onTap: () {
                               _selectedField(5);
                               print("tap 6");
@@ -209,19 +221,22 @@ class _TicTacToeState extends State<TicTacToe> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[6], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[6], animation: animated),
                             onTap: () {
                               _selectedField(6);
                               print("tap 7");
                             }),
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[7], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[7], animation: animated),
                             onTap: () {
                               _selectedField(7);
                               print("tap 8");
                             }),
                         GestureDetector(
-                            child: CustomCard(symbol: symbols[8], animation: animated),
+                            child: CustomCard(
+                                symbol: symbols[8], animation: animated),
                             onTap: () {
                               _selectedField(8);
                               print("tap 9");
@@ -233,28 +248,28 @@ class _TicTacToeState extends State<TicTacToe> {
               ),
             ),
             SizedBox(height: 20),
-            if(_checkWinner() != "")
-            Expanded(
-              flex: 1,
-              child: MaterialButton(
-                elevation: 15,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                color: Colors.lightGreenAccent,
-                minWidth: MediaQuery.of(context).size.width/2,
-                onPressed: () {
-                  _clearBoard();
-                },
-                child: Text("Zagraj Jeszcze Raz",
-                style: TextStyle(
-                  fontSize: 30,
-                ),),
-
-              ),
-            )
-            else
+            if (_checkWinner() != "" || selectedCount == 9)
               Expanded(
-                  flex: 1,
-                  child: SizedBox(height: 10)),
+                flex: 1,
+                child: MaterialButton(
+                  elevation: 15,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.lightGreenAccent,
+                  minWidth: MediaQuery.of(context).size.width / 2,
+                  onPressed: () {
+                    _clearBoard();
+                  },
+                  child: Text(
+                    "Zagraj Jeszcze Raz",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              )
+            else
+              Expanded(flex: 1, child: SizedBox(height: 10)),
             SizedBox(height: 50)
           ],
         ),
